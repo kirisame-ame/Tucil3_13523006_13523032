7f2086bdec3b88a4a2ddb0e5822566c42f08cbdd
William Andrian Dharma T <williamadt123@gmail.com>
2025-05-17 18:41:23 +0700

    init: Spring Boot

---
+++ rush_solver/src/main/java/kirisame/rush_solver/RushSolverApplication.java
@@ -0,0 +1,13 @@
+package kirisame.rush_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushSolverApplication.class, args);
+	}
+
+}
---
+++ rush_solver/src/test/java/kirisame/rush_solver/RushSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package kirisame.rush_solver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
