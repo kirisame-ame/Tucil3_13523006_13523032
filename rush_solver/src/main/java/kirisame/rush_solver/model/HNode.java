fae8e4636e4aa8881ca40fb862b90a4b11114393
19623248Git <19623248@std.stei.itb.ac.id>
2025-05-19 16:54:23 +0700

    feat: :sparkles: heuristic Node for A*

---
+++ rush_solver/src/main/java/kirisame/rush_solver/model/HNode.java
@@ -0,0 +1,51 @@
+package kirisame.rush_solver.model;
+
+public class HNode extends Node {
+
+    private int f;
+
+    public HNode(Node node, int h) {
+
+        // f(n) = g(n) + h(n)
+        // g(n) = depth
+        super(node.getBoard(), node.getParent(), node.getDepth());
+        this.f = node.getDepth() + h;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    /**
+     * Compares this node with another node based on the f value.
+     * This is used for sorting the priority queue.
+     * 
+     * @param other
+     * @return
+     */
+    public int compareTo(HNode other) {
+        if (this.f == other.getF()) {
+            return 0;
+        }
+        if (this.f > other.getF()) {
+            return 1;
+        }
+        return -1;
+    }
+
+    /**
+     * Compares this Hnode with another Hnode based on the f value and Board object.
+     * 
+     * @param other
+     * @return
+     */
+    public boolean equals(HNode other) {
+        // compare f value
+        if (this.f != other.getF()) {
+            return false;
+        }
+
+        // compare board
+        return (this.getBoard().equals(other.getBoard()));
+    }
+}
