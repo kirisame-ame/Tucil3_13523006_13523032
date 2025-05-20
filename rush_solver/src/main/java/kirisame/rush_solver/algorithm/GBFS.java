00959a383d9f6afb39c082d797675ef9788e9b82
William Andrian Dharma T <williamadt123@gmail.com>
2025-05-20 14:34:53 +0700

    feat: GBFS

--- rush_solver/src/main/java/kirisame/rush_solver/algorithm/GBFS.java
+++ rush_solver/src/main/java/kirisame/rush_solver/algorithm/GBFS.java
@@ -5,29 +5,35 @@ import java.util.PriorityQueue;
 
 import kirisame.rush_solver.model.Node;
 public class GBFS extends AbstractSearch {
-    public static ArrayList<Node> solve(Node initNode){
-        PriorityQueue<Node> openNodes = new PriorityQueue<>(Comparator.comparingInt(Node::getHeuristicValue));
-        ArrayList<Node> movesTaken = new ArrayList<>();
-        openNodes.add(initNode);
-        while (!openNodes.isEmpty()){
-            Node currentNode = openNodes.poll();
-            System.out.println("Current node: " + currentNode.getBoard().boardToString());
-            System.out.println("Heuristic value: " + currentNode.getHeuristicValue());
-            movesTaken.add(currentNode);
+    private PriorityQueue<Node> open;
+    private ArrayList<Node> closed;
+
+    public GBFS(Node startNode) {
+        Comparator<Node> comparator = Comparator.comparingInt(Node::getHeuristicValue);
+        open = new PriorityQueue<>(comparator);
+        closed = new ArrayList<>();
+        open.add(startNode);
+    }
+    @Override
+    public ArrayList<Node> solve(){
+        while (!open.isEmpty()){
+            Node currentNode = open.poll();
+            closed.add(currentNode);
             if (currentNode.getBoard().isGoal()){
                 System.out.println("Found solution");
-                return movesTaken;
+                buildPath(currentNode);
+                return this.path;
             }
             Node[] children = currentNode.expand();
             for (Node child : children){
-                if (!containsBoard(openNodes, child) ){
-                    openNodes.add(child);
-                }else if(!containsBoard(movesTaken,child)){
-                    openNodes.add(child);
+                if (!containsBoard(open, child) && !containsBoard(closed, child)){
+                    open.add(child);
                 }
             }
         }
         System.out.println("No solution found");
-        return null;
+        this.path = null; // No solution found
+        return this.path;
     }
+    
 }
